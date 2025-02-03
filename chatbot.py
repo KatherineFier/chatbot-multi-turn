1feb7842786e4fdd0d32038b285d5d9803ba16c9
katherine_fier <krfier@gmail.com>
2025-02-03 16:44:54 -0700

    imported titoken and added logic to limit token usage

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,26 @@
 from openai import OpenAI
+import tiktoken
 
 client = OpenAI()
 
-def get_api_chat_response_message(model, messages):
 
+def get_api_chat_response_message(model, messages):
     api_response = client.chat.completions.create(
-        model = model,
-        messages = messages
+        model=model,
+        messages=messages
     )
     # print(api_response)
 
     return api_response.choices[0].message.content
 
+
 model = "gpt-3.5-turbo"
+
+encoding = tiktoken.encoding_for_model(model)
+
+token_input_limit = 12289
+
+print(encoding)
 
 chat_history = []
 
@@ -24,6 +32,13 @@ while True:
 
     if user_input.lower() == "exit":
         break
+
+    token_count = len(encoding.encode(user_input))
+    # print(token_count)
+
+    if (token_count > token_input_limit):
+        print("Your prompt is too long. Please try again.")
+        continue
 
     chat_history.append({
         "role": "user",
